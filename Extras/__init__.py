4b87520f03e8ecc35f92d9879f7c68334fe6d8c8
freedomischaos <rickmand@gmail.com>
2014-07-17 19:22:15 -0400

    Update __init__.py

--- Extras/__init__.py
+++ Extras/__init__.py
@@ -29,8 +29,7 @@
 ###
 
 """
-Add a description of the plugin (to be presented to the user inside the wizard)
-here.  This should describe *what* the plugin does.
+Extra commands to make life easier and better for players and Storytellers.
 """
 
 import supybot
@@ -41,7 +40,7 @@ import supybot.world as world
 __version__ = ""
 
 # XXX Replace this with an appropriate author or supybot.Author instance.
-__author__ = supybot.authors.unknown
+__author__ = supybot.Author('David Rickman', 'freedomischaos', 'rickmand@gmail.com')
 
 # This is a dictionary mapping supybot.Author instances to lists of
 # contributions.
