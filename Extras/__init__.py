c29a078f0443cf56dda740b25d1dd9590802cd53
David Rickman <rickmand@gmail.com>
2014-10-09 20:12:13 -0400

    author stuffs

--- Extras/__init__.py
+++ Extras/__init__.py
@@ -47,7 +47,7 @@ __author__ = supybot.Author('David Rickman', 'freedomischaos', 'rickmand@gmail.c
 __contributors__ = {}
 
 # This is a url where the most recent plugin package can be downloaded.
-__url__ = '' # 'http://supybot.com/Members/yourname/Extras/download'
+__url__ = 'https://github.com/Elysia-Online/caine-bot'
 
 import config
 import plugin
