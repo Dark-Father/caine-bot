0f30200d33b14a4c7eddb248e215d7ca54f0f861
David Rickman <rickmand@gmail.com>
2014-10-12 19:35:38 -0400

    fixed Megan logic

--- Extras/plugin.py
+++ Extras/plugin.py
@@ -101,19 +101,20 @@ class Extras(callbacks.Plugin):
         try:
             if currentChannel == only_channel:
                 die = random.randint(1, 100)
-                if msg.nick == "Isabel" or msg.nick == "Megan":
-                    self.megan += 1
-                    silly_megan = "Megan slips and falls into the sacrifice instead, " \
-                                  "sacrificing herself to herself. Standard Megan move. " \
-                                  "Megan has been sacrificed %s times." % self.megan
-                    irc.reply(silly_megan, prefixNick=False)
-                elif megan != "Megan":
-                    irc.reply("Caine prefers Megan only.", prefixNick=False)
-                elif megan == "Megan":
-                    self.megan += 1
-                    irc.reply("Megan has been sacrificed to appeased Caine. Megan has been sacrificed %s times."
-                              % self.megan, prefixNick=False)
-                elif not megan:
+                if megan:
+                    if msg.nick == "Isabel" or msg.nick == "Megan":
+                        self.megan += 1
+                        silly_megan = "Megan slips and falls into the sacrifice instead, " \
+                                      "sacrificing herself to herself. Standard Megan move. " \
+                                      "Megan has been sacrificed %s times." % self.megan
+                        irc.reply(silly_megan, prefixNick=False)
+                    elif megan != "Megan":
+                        irc.reply("Caine prefers Megan only.", prefixNick=False)
+                    elif megan == "Megan":
+                        self.megan += 1
+                        irc.reply("Megan has been sacrificed to appeased Caine. Megan has been sacrificed %s times."
+                                  % self.megan, prefixNick=False)
+                if not megan:
                     if die < 90:
                         irc.reply("%s sacrifices themselves to %s."
                                   % (msg.nick, irc.nick), prefixNick=False)
